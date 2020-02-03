4247f36c04bc74b161557a08181c7fed669e8500
Neoamos <aonews@gmail.com>
2020-02-03 08:35:51 -0600

    bla

--- src/main/java/AirportHangar.java
+++ src/main/java/AirportHangar.java
@@ -2,7 +2,10 @@ import javax.swing.DefaultListModel;
 import javax.swing.JLabel;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class AirportHangar implements Runnable {
 
@@ -10,7 +13,10 @@ public class AirportHangar implements Runnable {
     String hangarName;
     int serverPort;
     Map<String, Integer> clientMap;
+    HashMap<String, ConcurrentLinkedQueue<Message>> outgoingMessageQueues = new HashMap<>();
     ArrayList<HangarClient> hangarClients = new ArrayList<>();
+    Random rand;
+
 
     JLabel jLabel;
     DefaultListModel<String> historyListModel;
@@ -22,11 +28,13 @@ public class AirportHangar implements Runnable {
         this.clientMap = clientMap;
         this.jLabel = jLabel;
         this.historyListModel = historyListModel;
+        this.rand = new Random();
         
         for (String client : this.clientMap.keySet()) {
             if (! this.hangarName.equals(client)) {
                 HangarClient hangarClient = new HangarClient(this, this.clientMap.get(client), client);
                 hangarClients.add(hangarClient);
+                outgoingMessageQueues.put(client, new ConcurrentLinkedQueue<>());
             }
         }
     }
@@ -35,6 +43,31 @@ public class AirportHangar implements Runnable {
     public void run() {
         new Thread(new PlaneShuffle(this)).start();
         new Thread(new HangarServer(this)).start();
+
+        int timeInterval = 1 + rand.nextInt(4);
+        while(true){
+            try {
+                Thread.sleep(timeInterval * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if (getAirplaneCount() > 0) {
+                int planeCount = 1 + rand.nextInt(5);
+
+                if (planeCount > getAirplaneCount()) {
+                    planeCount = getAirplaneCount();
+                }
+
+                HangarClient receiverClient = hangarClients.get(rand.nextInt(this.hangarClients.size()));
+                Message message = new Message(hangarName, planeCount, false);
+                outgoingMessageQueues.get(receiverClient.serverName).add(message);
+                setAirplaneCount(getAirplaneCount() - planeCount);
+            }
+
+            timeInterval = 1 + rand.nextInt(4);
+        }
+
     }
 
     public boolean areAllChannelsEmpty() {
--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -4,10 +4,12 @@ public class Message implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    private String senderName;
     private int airplaneCount;
     private boolean isMarkerMessage;
 
-    public Message(int airplaneCount, boolean isMarkerMessage) {
+    public Message(String senderName, int airplaneCount, boolean isMarkerMessage) {
+        this.senderName = senderName;
         this.airplaneCount = airplaneCount;
         this.isMarkerMessage = isMarkerMessage;
     }
